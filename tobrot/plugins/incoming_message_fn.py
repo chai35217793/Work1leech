b8d160d7e8ec899ac6f1915c2bde8a83fb22fb2d
chai35217793 <149489960+chai35217793@users.noreply.github.com>
2024-09-01 17:49:40 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -45,7 +45,7 @@ async def incoming_message_f(client, message):
     user_command = message.command[0]
     g_id = message.from_user.id
     credit = await message.reply_text(
-        f"🧲 Leeching for you <a href='tg://user?id={g_id}'>🤕</a>", parse_mode="markdown"
+        f"🧲 Leeching for you <a href='tg://user?id={g_id}'>🤕</a>", parse_mode="MarkDownV2"
     )
     # get link from the incoming message
     i_m_sefg = await message.reply_text("processing...", quote=True)
